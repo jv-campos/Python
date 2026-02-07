c6244154cfd13473a25d32c3a3c31841f86cebab
jv-campos <joaovictorcamposdossantos63@gmail.com>
2026-02-07 15:05:38 -0300

    Update ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,56 +1,37 @@
-from random import choice
+from random import randint
 from time import sleep
-pontos = 0 
-print('Olá, eu me chamo Hugo')
-print('Vamos jogar pedra, papel e tesolra ? ')
-jogo = int(input('Se quider digite 1 se não quiser digite 2: '))
 
-if jogo == 1:
-    while pontos < 1:
-        jogo = [1, 2, 3]
-        print("""
-            [ 1 ] Predra 
-            [ 2 ] Papel
-            [ 3 ] Tesolra
-            """)
-        jogador = int(input('Qual você vai jogar? '))
-        Hugo = choice(jogo)
-        print('Calma que estou escolhendo...')
-        sleep(2)
-        print('')
-        if Hugo == jogador:
-            print('Hugo escolheu {}, e você esoclheu {}. Empate'.format(Hugo, jogador))
-        elif Hugo == 1 and jogador == 2:
-            print('Hugo escolheu {}, e você esoclheu {}. Você ganhou!!!'.format(Hugo, jogador))
-            pontos += 1
-            break
-        elif Hugo == 1 and jogador == 3:
-            print('Hugo escolheu {}, e você esoclheu {}. Hugo ganhou!!!'.format(Hugo, jogador))
-            pontos += 1 
-            break
-        elif Hugo == 2 and jogador == 1:
-            print('Hugo escolheu {}, e você esoclheu {}. Hugo ganhou!!!'.format(Hugo, jogador))
-            pontos += 1
-            break
-        elif Hugo == 2 and jogador == 3:
-            print('Hugo escolheu {}, e você esoclheu {}. Você ganhou!!!'.format(Hugo, jogador))
-            pontos += 1 
-            break
-        elif jogador == 1 and Hugo == 2:
-            print('Hugo escolheu {}, e você esoclheu {}. Você ganhou!!!'.format(Hugo, jogador))
-            pontos += 1
-            break
-        elif jogador == 1 and Hugo == 3:
-            print('Hugo escolheu {}, e você esoclheu {}. Hugo ganhou!!!'.format(Hugo, jogador))
-            pontos += 1 
-            break
-        elif jogador == 2 and Hugo == 1:
-            print('Hugo escolheu {}, e você esoclheu {}. Hugo ganhou!!!'.format(Hugo, jogador))
-            pontos += 1
-            break
-        elif jogador == 2 and Hugo == 3:
-            print('Hugo escolheu {}, e você esoclheu {}. Você ganhou!!!'.format(Hugo, jogador))
-            pontos += 1 
-            break
-elif jogo == 2:
-    print('Ok, Vamos jogar outro dia')
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0,2)
+print('''Qual sua escolha
+[ 0 ] Pedra 
+[ 1 ] Papel
+[ 2 ] Tesoura
+''')
+jogada = int(input('Digite a sua escolha: '))
+print('-=' * 12)
+print('O computador jogou {} \nVocê jogou {}'.format(itens[computador], itens[jogada]))
+print('-=' * 12)
+if jogada == 0: #pedra
+    if computador == 0:
+        print('Empate')
+    elif computador == 1:
+        print('Computador venceu')
+    elif computador == 2:
+        print('Você venceu')
+elif jogada == 1:#papel
+    if computador == 0:
+        print('Você venceu')
+    elif computador == 1:
+        print('Empate')
+    elif computador== 2:
+        print('computador venceu') 
+elif jogada == 2: #tesoura 
+    if computador == 0:
+        print('computador venceu')
+    elif computador == 1:
+        print('Você venceu')
+    elif computador== 2:
+        print('Empate')
+else:
+    print('Jogada invalida')
